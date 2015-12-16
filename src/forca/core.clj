9441d31b226f83508ff35cf733d6c6c9bf5a20dd
Lemefe <lemefe21@gmail.com>
2015-12-16 21:54:33 -0200

    Implementação de novas funções(remove, contains, empty) no jogo da forca.

--- src/forca/core.clj
+++ src/forca/core.clj
@@ -3,6 +3,37 @@
 
 (def total-de-vidas 6)
 
+(defn perdeu [] (print "Voce perdeu..."))
+(defn ganhou [] (print "Voce ganhou!!!"))
+
+(defn letras-faltantes [palavra acertos]
+	(remove (fn [letra] (contains? acertos (str letra)) )
+		palavra
+	)
+)
+
+(defn acertou-a-palavra-toda? [palavra acertos]
+	(empty? (letras-faltantes palavra acertos))
+)
+
+(defn jogo [vidas palavra acertos] 
+	(if (= vidas 0)
+		(perdeu)
+		(if (acertou-a-palavra-toda? palavra acertos)
+			(ganhou)
+				(print "Chuta, amigo!")
+		)
+	)
+)
+
+(defn fibonacci [n]
+	(if (= n 0) 0
+		(if (= n 1) 1
+			(+ (fibonacci (- n 1)) (fibonacci (- n 2)) )
+		)
+	)
+)
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
