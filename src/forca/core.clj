6ded0e47d0f8337f0639bbc4d6a0423a66935ff7
Lemefe <lemefe21@gmail.com>
2016-01-04 20:37:07 -0200

    Implementação de funções recursivas e recursão de cauda na função jogo.

--- src/forca/core.clj
+++ src/forca/core.clj
@@ -20,22 +20,20 @@
 
 (defn acertou? [chute palavra] (.contains palavra chute))
 
-(defn avalia-chute [chute vidas palavra acertos]
-    (if (acertou? chute palavra)
-        (jogo vidas palavra (conj acertos chute))
-        (jogo (dec vidas) palavra acertos)
-    )
-)
-
 (defn jogo [vidas palavra acertos] 
-	(if (= vidas 0)
-		(perdeu)
-		(if (acertou-a-palavra-toda? palavra acertos)
-			(ganhou)
-			(avalia-chute (le-letra!) vidas palavra acertos)
-		)
-	)
-)
+	(cond
+		(= vidas 0) (perdeu)
+		(acertou-a-palavra-toda? palavra acertos) (ganhou)
+		:else
+		(let [chute (le-letra!)]
+			(if (acertou? chute palavra)
+				(do
+					(println "Acertou a letra!")
+					(recur vidas palavra (conj acertos chute)))
+				(do
+					(println "Errou a letra... Perdeu uma vida!")
+					(recur (dec vidas) palavra acertos))))))
+
 
 (defn fibonacci [n]
 	(if (= n 0) 0
