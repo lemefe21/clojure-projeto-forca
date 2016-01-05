437a1c291f9bbde7510193bdc5685301f54a39a6
Lemefe <lemefe21@gmail.com>
2016-01-05 20:52:58 -0200

    Criação da função para mostrar os resultados do jogo, trocando o map e funções preguiçosas pelo doseq.

--- src/forca/core.clj
+++ src/forca/core.clj
@@ -1,26 +1,32 @@
 (ns forca.core
   (:gen-class))
 
+
 (def total-de-vidas 6)
 
 (defn perdeu [] (print "Voce perdeu..."))
 (defn ganhou [] (print "Voce ganhou!!!"))
 
 (defn letras-faltantes [palavra acertos]
-	(remove (fn [letra] (contains? acertos (str letra)) )
-		palavra
-	)
-)
+	(remove (fn [letra] (contains? acertos (str letra))) palavra))
 
 (defn acertou-a-palavra-toda? [palavra acertos]
-	(empty? (letras-faltantes palavra acertos))
-)
+	(empty? (letras-faltantes palavra acertos)))
 
 (defn le-letra! [] (read-line))
 
 (defn acertou? [chute palavra] (.contains palavra chute))
 
-(defn jogo [vidas palavra acertos] 
+(defn imprime-forca [vidas palavra acertos]
+	(println "Vidas: " vidas)
+	(doseq [letra (seq palavra)]
+		(if (contains? acertos (str letra))
+			(print letra " ")
+			(print "_" " ")))
+	(println))
+
+(defn jogo [vidas palavra acertos]
+	(imprime-forca vidas palavra acertos)
 	(cond
 		(= vidas 0) (perdeu)
 		(acertou-a-palavra-toda? palavra acertos) (ganhou)
